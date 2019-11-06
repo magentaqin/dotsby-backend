a85733929910ca3fcb78919ce19a398f3d1e9e79
Mu Qin <magenta2127@gmail.com>
2019-11-06 20:11:48 +0800

    split create document query schema

--- app/modules/document/schema.js
+++ app/modules/document/schema.js
@@ -1,6 +1,115 @@
 const { Validator } = require('jsonschema');
 
 const v = new Validator();
+const sectionsSchemaId = "/document/sections";
+const pagesSchemaId = "/document/sections/pages";
+
+const sampleDocument = {
+  document_token: '1qazxsw2',
+  version: 0.1,
+  doc_title: 'Sample Doc',
+  sections: [
+    {
+      section_title: 'User Section',
+      pages: [
+        {
+          page_title: 'User Section Description',
+          is_root_path: true,
+          path: '/user',
+          content: '<h1>This part is written by John.</h1>'
+        },
+        {
+          page_title: 'Login',
+          path: '/user/login',
+          content: '<h1>Login Api</h1>',
+        }
+      ],
+    },
+    {
+      section_title: 'Account Section',
+      path: '/account',
+      pages: [
+        {
+          page_title: 'Create Account',
+          path: '/account/create',
+          content: '<h1>Create Account Api</h1>',
+        },
+        {
+          page_title: 'Get Account Info',
+          path: '/account/info',
+          content: '<h1>Get Account Info Api</h1>',
+        }
+      ]
+    }
+  ]
+}
+
+const pagesSchema = {
+  "id": pagesSchemaId,
+  "type": "array",
+  "description": "pages of section",
+  "minItems": 1,
+  "items": {
+    "type": "object",
+    "required": [
+      "page_title",
+      "path",
+      "content"
+    ],
+    "properties": {
+      "page_title": {
+        "type": "string",
+        "minLength": 1,
+        "maxLength": 50,
+        "format": "ascii_printable",
+        "description": "title of page"
+      },
+      "is_root_path": {
+        "type": "boolean",
+        "description": "whether this page is the index page"
+      },
+      "path": {
+        "type": "string",
+        "minLength": 1,
+        "maxLength": 32,
+        "format": "ascii_printable",
+        "description": "page path"
+      },
+      "content": {
+        "type": "string",
+        "minLength": 1,
+        "format": "ascii_printable",
+        "description": "page content"
+      }
+    }
+  }
+}
+
+const sectionsSchema = {
+  "id": sectionsSchemaId,
+  "type": "array",
+  "description": "sections of document",
+  "minItems": 1,
+  "items": {
+    "type": "object",
+    "required": [
+      "section_title",
+      "pages"
+    ],
+    "properties": {
+      "section_title": {
+        "type": "string",
+        "minLength": 1,
+        "maxLength": 50,
+        "format": "ascii_printable",
+        "description": "title of section"
+      },
+      "pages": {
+        "$ref": pagesSchemaId,
+      }
+    }
+  }
+}
 
 const createDocumentQuerySchema = {
   "type": "object",
@@ -31,68 +140,12 @@ const createDocumentQuerySchema = {
       "description": "title of document"
     },
     "sections": {
-      "type": "array",
-      "description": "sections of document",
-      "minItems": 1,
-      "items": {
-        "type": "object",
-        "required": [
-          "section_title",
-          "pages"
-        ],
-        "properties": {
-          "section_title": {
-            "type": "string",
-            "minLength": 1,
-            "maxLength": 50,
-            "format": "ascii_printable",
-            "description": "title of section"
-          },
-          "pages": {
-            "type": "array",
-            "description": "pages of section",
-            "minItems": 1,
-            "items": {
-              "type": "object",
-              "required": [
-                "page_title",
-                "path",
-                "content"
-              ],
-              "properties": {
-                "page_title": {
-                  "type": "string",
-                  "minLength": 1,
-                  "maxLength": 50,
-                  "format": "ascii_printable",
-                  "description": "title of page"
-                },
-                "is_root_path": {
-                  "type": "boolean",
-                  "description": "whether this page is the index page"
-                },
-                "path": {
-                  "type": "string",
-                  "minLength": 1,
-                  "maxLength": 32,
-                  "format": "ascii_printable",
-                  "description": "page path"
-                },
-                "content": {
-                  "type": "string",
-                  "minLength": 1,
-                  "format": "ascii_printable",
-                  "description": "page content"
-                }
-              }
-            }
-          }
-        }
-      }
+      "$ref": sectionsSchemaId
     }
   }
 }
 
-
-const errors = v.validate(sampleDocument, createDocumentQuerySchema)
-console.log(errors)
+v.addSchema(pagesSchema, pagesSchemaId)
+v.addSchema(sectionsSchema, sectionsSchemaId)
+const validationResult = v.validate(sampleDocument, createDocumentQuerySchema)
+console.log(validationResult.errors)

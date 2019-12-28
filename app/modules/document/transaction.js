b583ccc8dd3093f8f12bfbd3b0a9967b08ab9998
Mu Qin <magenta2127@gmail.com>
2019-12-28 22:10:26 +0800

    extract insert and update doc query from transaction flow

--- app/modules/document/transaction.js
+++ app/modules/document/transaction.js
@@ -5,6 +5,33 @@ const connection = require('@app/db/init');
 const Logger = require('@app/utils/logger');
 const { GlobalErrorCodes } = require('@app/utils/errorMessages');
 
+const insertNewDocQuery = (insertedDoc, document_id, user_id) => {
+  const docInsertSql = `INSERT INTO docs(document_id,version,title,is_published,created_at,updated_at,user_id,email)
+                        VALUES(?,?,?,?,?,?,?,?)`;
+  return new Promise((resolve, reject) => {
+    connection.query(docInsertSql, insertedDoc, (error, results) => {
+      if (error) {
+        Logger.error('insert new doc err: ', error, document_id, user_id)
+        reject(new Error(GlobalErrorCodes.SERVER_ERROR))
+      }
+      resolve({ data: results });
+    })
+  })
+}
+
+const updateDocQuery = (updatedDoc, document_id, user_id) => {
+  const docUpdateSql = 'UPDATE docs SET title = ?, updated_at = ?, is_published = ? WHERE id = ?';
+  return new Promise((resolve, reject) => {
+    connection.query(docUpdateSql, updatedDoc, (error, results) => {
+      if (error) {
+        Logger.error('update doc err: ', error, document_id, user_id)
+        reject(new Error(GlobalErrorCodes.SERVER_ERROR))
+      }
+      resolve({ data: results });
+    })
+  })
+}
+
 const publishNewDoc = (docData, sectionData, isNewVersion) => {
   /**
    * config doc data
@@ -13,14 +40,11 @@ const publishNewDoc = (docData, sectionData, isNewVersion) => {
     document_id, version, title, user_id, email,
   } = docData;
   const now = formatUTCDatetime();
-  const docInsertSql = `INSERT INTO docs(document_id,version,title,is_published,created_at,updated_at,user_id,email)
-               VALUES(?,?,?,?,?,?,?,?)`;
   const insertedDoc = [document_id, version, title, true, now, now, user_id, email];
-  const docUpdateSql = 'UPDATE docs SET title = ?, updated_at = ?, is_published = ? WHERE id = ?';
   const updatedDoc = [title, now, true, docData.id];
 
   return new Promise((resolve, reject) => {
-    connection.beginTransaction((err) => {
+    connection.beginTransaction(async (err) => {
       if (err) {
         Logger.error('publish new doc transaction err: ', err)
         reject(new Error(GlobalErrorCodes.SERVER_ERROR))
@@ -34,32 +58,31 @@ const publishNewDoc = (docData, sectionData, isNewVersion) => {
        * 2) if the version existed already && it has not been published:update title in docs and insert rows in sections/pages/api_items table.
        */
       if (isNewVersion) {
-        connection.query(docInsertSql, insertedDoc, (error, results) => {
-          if (error) {
-            return connection.rollback(() => {
-              Logger.error('insert new doc err: ', error, document_id, user_id)
-              reject(new Error(GlobalErrorCodes.SERVER_ERROR))
-            })
-          }
-          fkDocId = results.insertId;
-        })
+        const resp = await insertNewDocQuery(insertedDoc, document_id, user_id).catch(() => {
+          connection.rollback(() => {
+            reject(new Error(GlobalErrorCodes.SERVER_ERROR))
+          })
+        });
+        if (resp) {
+          fkDocId = resp.data.results.insertId;
+        }
       } else {
-        connection.query(docUpdateSql, updatedDoc, (error, results) => {
-          if (error) {
-            return connection.rollback(() => {
-              Logger.error('update err: ', error, document_id, user_id)
-              reject(new Error(GlobalErrorCodes.SERVER_ERROR))
-            })
-          }
-          fkDocId = results.insertId;
+        const resp = await updateDocQuery(updatedDoc, document_id, user_id).catch(() => {
+          connection.rollback(() => {
+            reject(new Error(GlobalErrorCodes.SERVER_ERROR));
+          })
         })
+        if (resp) {
+          fkDocId = resp.data.results.insertId;
+        }
       }
 
       /**
       * config page data and section data
       */
       const pages = [];
-      const sections = sectionData.map((item, index) => {
+      const sections = [];
+      sectionData.forEach((item, index) => {
         const sectionId = hashHelper({ section_title: item.title, order_index: index, created_at: now });
 
         const pageInfo = item.pages.map(pageItem => {
@@ -85,23 +108,40 @@ const publishNewDoc = (docData, sectionData, isNewVersion) => {
           return pageObj;
         })
 
-        return {
-          title: item.title,
-          order_index: index,
-          section_id: sectionId,
-          doc_id: fkDocId,
-          created_at: now,
-          updated_at: now,
-          page_info: JSON.stringify(pageInfo),
-        }
+        sections.push([
+          sectionId,
+          item.title,
+          index,
+          JSON.stringify(pageInfo),
+          now,
+          now,
+          fkDocId,
+        ]);
       })
 
 
-      // insert new row to sections and pages table
+      // insert rows to sections table
+      const sectionsInsertSql = `INSERT INTO sections(section_id,title,order_index,page_info,created_at,updated_at,doc_id) VALUES ? `;
+      connection.query(sectionsInsertSql, [sections], (error, results) => {
+        if (error) {
+          return connection.rollback(() => {
+            Logger.error('insert sections err: ', error, fkDocId, JSON.stringify(sections))
+            reject(new Error(GlobalErrorCodes.SERVER_ERROR))
+          })
+        }
+      });
+
+
+      // insert rows to pages table
     })
   })
 }
 
 const updatePublish = () => {
 
+}
+
+module.exports = {
+  publishNewDoc,
+  updatePublish,
 }

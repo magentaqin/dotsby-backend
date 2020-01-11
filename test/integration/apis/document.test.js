3438a18490c7a17239d49d91621beae0b3c62882
Mu Qin <magenta2127@gmail.com>
2020-01-11 15:24:25 +0800

    add test for document list api

--- test/integration/apis/document.test.js
+++ test/integration/apis/document.test.js
@@ -6,6 +6,7 @@ const userRouter = require('@app/modules/user/router')
 const { mockedDocToPublish } = require('@test/sampleData')
 const publishDocumentResponseSchema = require('@schema/src/apis/document_publish_response')
 const getDocumentInfoResponseSchema = require('@schema/src/apis/document_info_response')
+const docsListResponseSchema = require('@schema/src/apis/document_list_response')
 const createDocumentSchema = require('@schema/src/apis/document_create_response')
 const errorResponseSchema = require('@schema/src/response/type_response_error')
 const { GlobalErrorCodes, DocErrorCodes } = require('@app/utils/errorMessages')
@@ -14,6 +15,7 @@ const validator = new Validator()
 
 const createDocumentUrl = router.url('createDocument')
 const publishDocumentUrl = router.url('publishDocument')
+const listDocsUrl = router.url('listDocument')
 const getDocInfoUrl = router.url('getDocumentInfo')
 const signupUrl = userRouter.url('signUp')
 
@@ -82,7 +84,7 @@ describe('Test Document APIS', async () => {
       assert(resp.data.code === GlobalErrorCodes.INVALID_PARAMETERS)
     })
 
-    // 401 TODO
+    // 401
     it('should return 401 status when unauthorized', async() => {
       const resp = await http.post(publishDocumentUrl);
       const result = validator.validate(resp.data.data, errorResponseSchema.schema)
@@ -90,13 +92,36 @@ describe('Test Document APIS', async () => {
       assert(resp.status === 401)
     })
 
-    // 403 TODO
+    // 403
     it('should return error code CREATE_BEFORE_PUBLUSH and 403 status when document_id not found in database', async() => {
       mockedDocToPublish.document_id = '%1234';
       const resp = await http.post(publishDocumentUrl, mockedDocToPublish, authHeader);
       assert(resp.status === 403)
       assert(validator.validate(resp.data, errorResponseSchema.schema).errors.length === 0)
       assert(resp.data.code === DocErrorCodes.CREATE_BEFORE_PUBLISH);
+    })
+  });
+
+
+  /**
+   * Get Document List Api
+   */
+  describe('List Document', async () => {
+    // 200 OK
+    it('should return docs array and 200 status when authorized', async() => {
+      const resp = await http.get(listDocsUrl, {}, authHeader)
+      const validationResult = validator
+        .validate(resp.data.data, docsListResponseSchema.schema);
+      assert(validationResult.errors.length === 0)
+      assert(resp.status === 200)
+    });
+
+    // 401
+    it('should return 401 status when unauthorized', async() => {
+      const resp = await http.get(listDocsUrl);
+      const result = validator.validate(resp.data.data, errorResponseSchema.schema)
+      assert(result.errors.length === 0)
+      assert(resp.status === 401)
     })
   });
 
